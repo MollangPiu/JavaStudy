036e6bc50c837bc6176cae18a58396b88bde1a9a
unknown <you@example.com>
2023-12-05 15:39:14 +0900

    Update: 변수명 자료 변경

--- src/campus/ch01/pro02/DataType.java
+++ src/campus/ch01/pro02/DataType.java
@@ -35,10 +35,6 @@ class DataType {
         System.out.println(in*160);
         System.out.println(in*-1*150);
 
-        System.out.println("==========================");
-        System.out.println(0111);
-        System.out.println(0xC);
-        System.out.println(0b101);
 
 
         System.out.println("==========================");
@@ -71,5 +67,23 @@ class DataType {
         System.out.println(str);
         System.out.println("Hello World");
         System.out.println("수고하셨습니다.");
+
+
+        System.out.println("==========================");
+        
+        //진법 표현하기
+        System.out.println(0b101);  //2진수
+        System.out.println(0111);   //8진수
+        System.out.println(0xC);    //16진수
+
+        System.out.println(0XA);
+        System.out.println(0xA);
+        System.out.println(0xa);
+
+
+        final String NAME = "상수를 저장합니다.";
+        //NAME = "final을 지정하게 되면, 해당 변수는 수정이 불가능 합니다.";
+
+        //static int number = 100;
     }
 }
