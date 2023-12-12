333be155540d3e02c50bf1e95937d9c2ee09f3d1
unknown <you@example.com>
2023-12-12 18:23:08 +0900

    Feat: New Class

---
+++ src/campus/ch04/NewClass.java
@@ -0,0 +1,17 @@
+package campus.ch04;
+
+class NewClass {
+
+    public static void main(String[] args) {
+
+        SecondClass secondClass = new SecondClass();
+
+
+    }
+}
+
+class SecondClass {
+
+    String ref;
+    int num;
+}
