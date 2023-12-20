ef9bec41e0e2cc70c7d8f77514e856a52348271a
unknown <you@example.com>
2023-12-20 11:10:29 +0900

    Message

---
+++ src/campus/ch01/pro02/DoubleType.java
@@ -0,0 +1,8 @@
+package campus.ch01.pro02;
+
+class DoubleType {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/campus/ch02/pro01/TestFor.java
@@ -0,0 +1,8 @@
+package campus.ch02.pro01;
+
+public class TestFor {
+
+    public static void main(String[] args) {
+        int [][] arr = {{1, 2, 3}, {1, 2, 3}, {1, 1,1}};
+    }
+}
