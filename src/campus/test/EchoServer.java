ed543ba37bbb495b7af8be3d472c2e8cc06de1d4
MHS <hayaruby7909@gmail.com>
2024-06-23 04:42:39 +0900

    Feat: network

---
+++ src/campus/test/EchoServer.java
@@ -0,0 +1,46 @@
+package campus.test;
+
+import java.io.*;
+import java.net.*; 
+public class EchoServer{ 
+	private BufferedReader bufferR; 
+	private BufferedWriter bufferW; 
+	private InputStream is; 
+	private OutputStream os; 
+	private ServerSocket serverS; 
+	public EchoServer(int port){ 
+		try{ 
+			serverS = new ServerSocket(port); 
+		}catch(IOException ioe){ 
+			ioe.printStackTrace(); 
+			System.exit(0); 
+		} 
+		while(true){ 
+			try{ 
+				System.out.println("실행 확인");
+				Socket tcpSocket = serverS.accept(); 
+				System.out.println("입장 IP : "+
+						tcpSocket.getInetAddress().getHostAddress()); 
+				is = tcpSocket.getInputStream(); 
+				os = tcpSocket.getOutputStream(); 
+				bufferR = new BufferedReader( 
+						new InputStreamReader(is)); 
+				bufferW = new BufferedWriter( 
+						new OutputStreamWriter(os)); 
+				String message = bufferR.readLine(); 
+				System.out.println("message : "+ message);
+				message += System.getProperty("line.separator"); 
+				bufferW.write(message); 
+				bufferW.flush(); 
+				bufferR.close();
+				bufferW.close();
+				tcpSocket.close();
+			}catch(IOException ioe){ 
+				ioe.printStackTrace(); 
+			} 
+		} 
+	} 
+	public static void main(String[] args){ 
+		new EchoServer(3000); 
+	} 
+} 
