1d35f7c34618091f58eb45e07660a054107f3066
unknown <you@example.com>
2024-07-07 15:44:10 +0900

    Update: 리스트 만들기

---
+++ src/campus/link/LinkNode.java
@@ -0,0 +1,15 @@
+package campus.link;
+
+class LinkNode {
+
+    private Student student;
+    LinkNode next;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+}
