0c53689934050a82b5a78fc3c9eae77d611b0c5a
unknown <you@example.com>
2024-07-07 23:42:30 +0900

    Feat: frame 추가

---
+++ src/campus/frameEx02/FrameEx1.java
@@ -0,0 +1,14 @@
+package campus.frameEx02;
+
+import java.awt.*;
+	
+public class FrameEx1{
+	public static void main(String args[]){
+		Frame f = new Frame();
+
+		f.setTitle("Frame Test");
+
+		f.setSize(300,300);
+		f.setVisible(true);
+	}
+}
