3a3b436166b340395ea4329094e198a8dd545f6a
unknown <you@example.com>
2024-07-11 22:07:27 +0900

    Feat: index문 완성

---
+++ src/campus/db/DBCheck.java
@@ -0,0 +1,111 @@
+package campus.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+/**
+ * 테스트용 DB
+ * CREATE TABLE MEMBER (
+ *     NUM INT AUTO_INCREMENT PRIMARY KEY,
+ *     COUNT INT,
+ *     NAME VARCHAR(30),
+ *     SCORE INT
+ * );
+ */
+public class DBCheck {
+
+    Connection conn;
+    PreparedStatement pstmt;
+    ResultSet rs;
+
+    public DBCheck() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/mydb?serverTimezone=UTC";
+            String user = "root";
+            String pw = "qwe123!@#";
+            conn = DriverManager.getConnection(url, user, pw);
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insert(int count) {
+
+
+        String sql = "INSERT INTO MEMBER (COUNT, NAME, SCORE) VALUES (?, ?, ?)";
+
+        try {
+            pstmt = conn.prepareStatement(sql);
+
+            for(int i=0; i< count; i++) {
+                System.out.println("i: "+i);
+                String name = RandomName.randomName();
+                int randomValue = (int) (Math.random() * 91 + 10);
+
+                pstmt.setInt(1, i);
+                pstmt.setString(2, name);
+                pstmt.setInt(3, randomValue);
+
+                int result = pstmt.executeUpdate();
+                System.out.println("결과: "+result);
+            }
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if(rs != null) rs.close();
+                if(pstmt != null) pstmt.close();
+                if(conn != null) conn.close();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void delete() {
+        String sql = "DELETE FROM MEMBER";
+
+        try {
+            pstmt = conn.prepareStatement(sql);
+
+            int result = pstmt.executeUpdate();
+
+            System.out.println("결과: "+result);
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if(rs != null) rs.close();
+                if(pstmt != null) pstmt.close();
+                if(conn != null) conn.close();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        DBCheck dbCheck = new DBCheck();
+
+        dbCheck.insert(2000000);
+
+
+
+       // dbCheck.delete();
+    }
+}
--- src/campus/db/DbConnection.java
+++ src/campus/db/DbConnection.java
@@ -1,4 +1,28 @@
 package campus.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DbConnection {
+
+    public static void main(String[] args) {
+
+        try {
+
+            String url = "jdbc:mysql://localhost:3306/mydb?serverTimezone=UTC";
+            String user = "root";
+            String pw = "qwe123!@#";
+            Connection conn = DriverManager.getConnection(url, user, pw);
+
+            System.out.println("123");
+
+            //연결 실패 시, 다시 재 연결
+            if(conn == null || conn.isClosed()) {
+                conn = DriverManager.getConnection(url, user, pw);
+                System.out.println("체크");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
