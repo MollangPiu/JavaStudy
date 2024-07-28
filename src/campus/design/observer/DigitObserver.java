33a300d072611b7ff15cba0ac13e410dd1872d8d
MHS <hayaruby7909@gmail.com>
2024-07-28 23:12:45 +0900

    Feat: Observer

---
+++ src/campus/design/observer/DigitObserver.java
@@ -0,0 +1,9 @@
+package campus.design.observer;
+
+public class DigitObserver implements Observer{
+
+    @Override
+    public void update(int number) {
+        System.out.println("number: "+number);
+    }
+}
