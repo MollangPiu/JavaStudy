ad750be56b42c74e75248bc25b5890d9c5888ad2
MHS <hayaruby7909@gmail.com>
2025-03-18 04:54:21 +0900

    Feat: ocp 적용

---
+++ src/campus/solid원칙/ocp/올바른예제.java
@@ -0,0 +1,45 @@
+package campus.solid원칙.ocp;
+
+public class 올바른예제 {
+
+    public static void main(String[] args) {
+        결제관리interface payManager = null;
+
+        //현금결제, 카드 결제 선택
+        결제프로세스 payProcess = new 결제프로세스(new 현금결제()); //현금 결제 선택
+        payProcess.결제프로세스();
+    }
+}
+
+interface 결제관리interface {
+    void pay();
+}
+
+//신용카드 결제
+class 현금결제 implements 결제관리interface {
+    @Override
+    public void pay() {
+        System.out.println("현금 결제");
+    }
+}
+
+//카드 결제
+class 카드결제 implements 결제관리interface {
+    @Override
+    public void pay() {
+        System.out.println("카드 결제");
+    }
+}
+
+class 결제프로세스 {
+    결제관리interface payManager;
+
+    public 결제프로세스(결제관리interface payManager) {
+        this.payManager = payManager;
+    }
+
+    public void 결제프로세스() {
+        payManager.pay();
+        System.out.println("결제 프로세스");
+    }
+}
---
+++ src/campus/solid원칙/ocp/잘못된예제.java
@@ -0,0 +1,22 @@
+package campus.solid원칙.ocp;
+
+public class 잘못된예제 {
+
+    public static void main(String[] args) {
+        결제관리 payProcess = new 결제관리();
+        payProcess.결제프로세스(1);
+    }
+}
+
+class 결제관리 {
+    public void 결제프로세스(int typeCnt) {
+        if(typeCnt == 0) {
+            System.out.println("현금 결제");
+        }
+        else if(typeCnt == 1) {
+            System.out.println("카드 결제");
+        }
+
+        System.out.println("결제 프로세스");
+    }
+}
